24abe9df42beaf657770a6a0be6b0609b3430b77
Oleg <lyerhd@gmail.com>
2023-05-03 23:36:53 +0300

    dotenv

--- recruto/lending/views.py
+++ recruto/lending/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect, reverse
 from django.views import generic
+import logging
+
+logger = logging.getLogger('recruto')
 
 
 class HelloView(generic.TemplateView):
@@ -7,11 +9,11 @@ class HelloView(generic.TemplateView):
 
     def get_context_data(self):
         super().get_context_data()
-        message= self.request.GET.get('message', 'No messages yet!')
-        name= self.request.GET.get('name', 'Noname')
-        context={
-            'message':message,
-            'name':name
+        message = self.request.GET.get('message', 'No messages yet!')
+        name = self.request.GET.get('name', 'Noname')
+        context = {
+            'message': message,
+            'name': name
         }
+        logger.info(f'RECRUTO WAS REQUESTED BY {self.request.META.get("REMOTE_ADDR")}')
         return context
-
--- recruto/recruto/settings.py
+++ recruto/recruto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(z2z@8&yq_5^9dk1on7*m4g41jnr&njx8^0yyy^m$_*%a&-zmc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = tuple(os.environ.get('ALLOWED_HOSTS', '*'))
 
 
 # Application definition
@@ -73,12 +74,12 @@ WSGI_APPLICATION = 'recruto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
@@ -121,3 +122,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "recruto_file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            'filename': BASE_DIR/'recruto.log'
+        },
+    },
+    "loggers": {
+        "recruto": {
+            "handlers": ["recruto_file"],
+            "level": "INFO",
+        },
+    },
+}
